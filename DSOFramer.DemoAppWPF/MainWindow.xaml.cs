654063b977ada915a45c5efe19b7e87188019747
prof79 <markus@markusegger.at>
2025-02-16 18:57:12 +0100

    Event handlers optimized/remove code duplication; why multiple buttons.

--- DSOFramer.DemoAppWPF/MainWindow.xaml.cs
+++ DSOFramer.DemoAppWPF/MainWindow.xaml.cs
@@ -41,11 +41,13 @@ namespace DSOFramer.DemoAppWPF
             LoadDSO();
         }
 
-        private void OpenWord_Click(object sender, RoutedEventArgs e)
+        private void OpenDocument_Click(object sender, RoutedEventArgs e)
         {
             var ofd = new System.Windows.Forms.OpenFileDialog();
             
-            ofd.Filter = "Microsoft Word Files|*.doc?";
+            ofd.Filter =
+                "Microsoft Office Files|*.doc?;*.xls?;*.ppt?;*.pps?|Microsoft Word Files|*.doc?|Microsoft Excel Files|*.xls?|Microsoft PowerPoint Files|*.ppt?;*.pps?";
+
             ofd.Multiselect = false;
 
             if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
@@ -68,34 +70,7 @@ namespace DSOFramer.DemoAppWPF
             }
         }
 
-        private void OpenExcel_Click(object sender, RoutedEventArgs e)
-        {
-            var ofd = new System.Windows.Forms.OpenFileDialog();
-
-            ofd.Filter = "Microsoft Excel Files|*.xls?";
-            ofd.Multiselect = false;
-
-            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
-            {
-                var filename = ofd.FileName;
-
-                MessageBox.Show(
-                    "Office embedding control could not be loaded.",
-                    "Error",
-                    MessageBoxButton.OK,
-                    MessageBoxImage.Error
-                );
-
-                dsoOffice.OpenDocument(filename);
-            }
-        }
-
-        private void SaveWord_Click(object sender, RoutedEventArgs e)
-        {
-            dsoOffice.SaveDocument();
-        }
-
-        private void SaveExcel_Click(object sender, RoutedEventArgs e)
+        private void SaveDocument_Click(object sender, RoutedEventArgs e)
         {
             dsoOffice.SaveDocument();
         }
